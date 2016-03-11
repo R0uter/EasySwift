dc2236a999cee8a4c5ecef5799df6beea510b134
stubbornness1219 <18223847097@163.com>
2016-03-11 17:58:15 +0800

    新建工程

---
+++ EasySwift/EasySwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasySwift
+//
+//  Created by yuanxiaojun on 16/3/11.
+//  Copyright © 2016年 袁晓钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasySwift/EasySwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasySwift
+//
+//  Created by yuanxiaojun on 16/3/11.
+//  Copyright © 2016年 袁晓钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
